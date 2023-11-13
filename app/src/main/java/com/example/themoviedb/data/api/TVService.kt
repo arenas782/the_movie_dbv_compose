05aad8942a280eb917b74d0304a22100cf385b29
Jose Arenas <jarenas@applaudostudios.com>
2023-11-13 16:53:06 -0400

    changing api key

--- app/src/main/java/com/example/themoviedb/data/api/TVService.kt
+++ app/src/main/java/com/example/themoviedb/data/api/TVService.kt
@@ -7,22 +7,26 @@ import retrofit2.http.*
 
 
 interface   TVService {
-    @GET("tv/popular?api_key=f52f4a0bd6f7db6d9bab750909ed1736&language=en-US")
+    @Headers("Authorization: Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxYTczYjI5NWQ5OGQzNTBlNWMwZWNkOTIyOWFmMjg3ZSIsInN1YiI6IjVmOTM4YmE1MmI5MzIwM2YyZjc0YzBmMyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.SbV9Zq6HY8BlaFNmbt8WqD-Q1xo_FjtiV9VgSF_t6Wc")
+    @GET("tv/popular?language=en-US")
     suspend fun popularTVShows(@Query("page") page : Int): LatestTVShowsResponse
-
-    @GET("tv/on_the_air?api_key=f52f4a0bd6f7db6d9bab750909ed1736&language=en-US")
+    @Headers("Authorization: Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxYTczYjI5NWQ5OGQzNTBlNWMwZWNkOTIyOWFmMjg3ZSIsInN1YiI6IjVmOTM4YmE1MmI5MzIwM2YyZjc0YzBmMyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.SbV9Zq6HY8BlaFNmbt8WqD-Q1xo_FjtiV9VgSF_t6Wc")
+    @GET("tv/on_the_air?language=en-US")
     suspend fun onTheAirTVShows(@Query("page") page : Int): LatestTVShowsResponse
-
-    @GET("tv/airing_today?api_key=f52f4a0bd6f7db6d9bab750909ed1736&language=en-US")
+    @Headers("Authorization: Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxYTczYjI5NWQ5OGQzNTBlNWMwZWNkOTIyOWFmMjg3ZSIsInN1YiI6IjVmOTM4YmE1MmI5MzIwM2YyZjc0YzBmMyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.SbV9Zq6HY8BlaFNmbt8WqD-Q1xo_FjtiV9VgSF_t6Wc")
+    @GET("tv/airing_today?language=en-US")
     suspend fun airingTodayTVShows(@Query("page") page : Int): LatestTVShowsResponse
+    @Headers("Authorization: Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxYTczYjI5NWQ5OGQzNTBlNWMwZWNkOTIyOWFmMjg3ZSIsInN1YiI6IjVmOTM4YmE1MmI5MzIwM2YyZjc0YzBmMyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.SbV9Zq6HY8BlaFNmbt8WqD-Q1xo_FjtiV9VgSF_t6Wc")
+    @GET("tv/top_rated?language=en-US")
 
-    @GET("tv/top_rated?api_key=f52f4a0bd6f7db6d9bab750909ed1736&language=en-US")
     suspend fun topRatedTVShows(@Query("page") page : Int): LatestTVShowsResponse
 
-    @GET("tv/{tvShowId}?api_key=f52f4a0bd6f7db6d9bab750909ed1736&language=en-US")
+    @Headers("Authorization: Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxYTczYjI5NWQ5OGQzNTBlNWMwZWNkOTIyOWFmMjg3ZSIsInN1YiI6IjVmOTM4YmE1MmI5MzIwM2YyZjc0YzBmMyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.SbV9Zq6HY8BlaFNmbt8WqD-Q1xo_FjtiV9VgSF_t6Wc")
+    @GET("tv/{tvShowId}?language=en-US")
     suspend fun getTVShowDetails(@Path("tvShowId") tvShowId : Int): TVShowDetailsResponse
 
-    @GET("search/tv?api_key=f52f4a0bd6f7db6d9bab750909ed1736&language=en-US&page=1&include_adult=false")
+    @Headers("Authorization: Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxYTczYjI5NWQ5OGQzNTBlNWMwZWNkOTIyOWFmMjg3ZSIsInN1YiI6IjVmOTM4YmE1MmI5MzIwM2YyZjc0YzBmMyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.SbV9Zq6HY8BlaFNmbt8WqD-Q1xo_FjtiV9VgSF_t6Wc")
+    @GET("search/tv?language=en-US&page=1&include_adult=false")
     suspend fun searchTVShows(@Query("query") query : String,@Query("page") page : Int): LatestTVShowsResponse
 
 }
--- app/src/main/java/com/example/themoviedb/ui/composables/ChipFilter.kt
+++ app/src/main/java/com/example/themoviedb/ui/composables/ChipFilter.kt
@@ -10,8 +10,8 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-
 @Composable
 fun ChipFilter(
     filterName : String,
